65e6a752677343f06c6f36f91ab1a6b7a38ee441
Brad Hambrice <hambrice@uab.edu>
2018-01-29 23:32:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  get 'recipes/:id' do
+  get '/recipes/:id' do
     @recipe = Recipe.find_by(id: params[:id])
     erb :show
   end
